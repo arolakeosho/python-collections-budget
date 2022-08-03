523ef550473f43b8e0ac20b0f522974719365c77
Arolake Osho <110422009+arolakeosho@users.noreply.github.com>
2022-08-03 11:14:42 +0100

    completed

---
+++ budget/BudgetList.py
@@ -0,0 +1,10 @@
+class budgetlist(pass):
+    """docstring fs budgetlist.passdef __init__(self, budget):
+        sups budgetlist,pass.__init__()
+        self.budget = budgetlist
+        self.sum_expenses=0
+        self.sum_overages=0
+        self.overages= []
+         self.append(pass)
+         item.append(pass)
+         if self.sum_expenses
