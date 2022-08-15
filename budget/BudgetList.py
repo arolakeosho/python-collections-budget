6cb7d3b9cd3085751915b710a84f3eb3ff1575b1
Arolake Osho <110422009+arolakeosho@users.noreply.github.com>
2022-08-15 15:44:44 +0100

    jj

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -5,11 +5,13 @@ import matplotlib.pyplot as plt
 class BudgetList():
         def __init__(self, budget):
             self.budget = budget
-            self.sum_expenses=0
+            self.sum_expenses = 0
             self.expenses= []
-            self.sum_overages=0
+            self.sum_overages = 0
             self.overages= []
 
+        def __len__(self):
+                return (len(self.expenses) + len(self.overages))
 
         def append(self, item):
              if (self.sum_expenses+item < self.budget):
@@ -19,9 +21,6 @@ class BudgetList():
                 self.overages.append(item)
                 self.sum_overages += item
 
-        def __len__(self):
-            return (len(self.expenses) + len(self.overages))
-
 def main():
     myBudgetList = BudgetList(1200)
     #add expenses, the last expense is 100 and that goes in overages
@@ -29,9 +28,7 @@ def main():
     expenses.read_expenses('data/spending_data.csv')
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
-
-
-print('The count of all expenses: ' + str(len(myBudgetList)))
+        print('The count of all expenses: ' + str(len(myBudgetList)))
 
 for entry in myBudgetList:
     print(entry)
--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -3,16 +3,19 @@ from . import Expense
 import matplotlib.pyplot as plt
 #create a list of spending categories
 
-expenses= Expense.Expenses()
+expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
+
 spending_categories= []
 for expense in expenses.list:
     spending_categories.append(expense.category)
 
 #Count Categories with a Counter Collection
 spending_counter= collections.Counter(spending_categories)
-top5= spending_counter.most_common(5)
+print(spending_counter)
+top5 = spending_counter.most_common(5)
 categories, count= zip(*top5)
+
 fig,ax=plt.subplots()
 ax.bar(categories, count)
 ax.set_title('# of Purchases by Category')
